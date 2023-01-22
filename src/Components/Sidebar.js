05b164b6bc449a013eab08811b421f5d1d9dd43c
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-01-22 23:36:57 +0530

    sidebar -upgrade

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { SiShopware } from 'react-icons/si'
-import { MdOutlineCancel } from 'react-icons/md'
+import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { links } from '../data/dummy'
 const Sidebar = () => {
-  
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-blue  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   const activeMenu = true
@@ -39,7 +39,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  
+
                   className={({ isActive }) => (isActive ? activeLink : normalLink)}
                 >
                   {link.icon}
