9cc74ab9ef3f2332561d67a8955e8724290f4998
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-01-28 23:50:22 +0530

    efficiency sidebar

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar></Sidebar>
+              <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
