b038b8d9415ddaeca427370012dc68a1b93b6f13
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-01-29 21:49:22 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { FiSettings } from 'react-icons/fi'
 import { Navbar, Footer, Sidebar, ThemeSettings } from './Components'
-import { Ecommerce, Orders, Calender,Line, Employees, Stacked, Pyramid, Customer, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './Pages'
+import { Ecommerce, Orders, Calender, Line, Employees, Stacked, Pyramid, Customer, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './Pages'
 import { useStateContext } from './Contexts/ContextProvider';
 
 
 const App = () => {
-  const {activeMenu} = useStateContext();
-  
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
@@ -38,27 +38,29 @@ const App = () => {
                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>Navbar</div>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
           </div>
           <div>
             <Routes>
-              <Route path='/' element={<Ecommerce/>} />
-              <Route path='/ecommerce' element={<Ecommerce/>} />
-              <Route path='/orders' element={<Orders/>} />
-              <Route path='/employees' element={<Employees/>} />
-              <Route path='/customer' element={<Customer/>} />
-              <Route path='/kanban' element={<Kanban/>} />
-              <Route path='/editor' element={<Editor/>} />
-              <Route path='/calender' element={<Calender/>} />
-              <Route path='/color-picker' element={<ColorPicker/>} />
-              <Route path='/area' element={<Area/>} />
-              <Route path='/pie' element={<Pie/>} />
-              <Route path='/line' element={<Line/>} />
-              <Route path='/bar' element={<Bar/>} />
-              <Route path='/financial' element={<Financial/>} />
-              <Route path='/pyramid' element={<Pyramid/>} />
-              <Route path='/color-mapping' element={<ColorMapping/>} />
-              <Route path='/stacked' element={<Stacked/>} />
+              <Route path='/' element={<Ecommerce />} />
+              <Route path='/ecommerce' element={<Ecommerce />} />
+              <Route path='/orders' element={<Orders />} />
+              <Route path='/employees' element={<Employees />} />
+              <Route path='/customer' element={<Customer />} />
+              <Route path='/kanban' element={<Kanban />} />
+              <Route path='/editor' element={<Editor />} />
+              <Route path='/calender' element={<Calender />} />
+              <Route path='/color-picker' element={<ColorPicker />} />
+              <Route path='/area' element={<Area />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/financial' element={<Financial />} />
+              <Route path='/pyramid' element={<Pyramid />} />
+              <Route path='/color-mapping' element={<ColorMapping />} />
+              <Route path='/stacked' element={<Stacked />} />
             </Routes>
           </div>
         </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { FiShoppingCart } from 'react-icons/fi'
+import { BsChatLeft } from 'react-icons/bs'
+
+import { RiNotification3Line } from 'react-icons/ri'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import avatar from '../data/avatar.jpg'
+import { Cart, Chat, Notifications, UserProfile } from '.'
+import { useStateContext } from '../Contexts/ContextProvider'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
-export default Navbar
+
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
+  return (
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+
+      <NavButton title="Menu" icon={<AiOutlineMenu />} />
+      <div className="flex">
+        <NavButton title="Cart" icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" icon={<BsChatLeft />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" icon={<RiNotification3Line />} />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
